0447cc5b9d8101fe46449c2f8deefc2ec8d20747
zahiriqbal94 <zahiriqbal94@gmail.com>
2024-05-06 16:50:51 +0500

    Demo commit

---
+++ src/Demo/Test_2.java
@@ -0,0 +1,17 @@
+package Demo;
+
+public class Test_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      String hungry="yes";
+      if(hungry=="yes") 
+       {
+    	 System.out.println("set something......."); 
+       }
+      else {
+    	  System.out.println("Do work.......");
+      }
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module JenkinsPipelineDemoProject {
+}
